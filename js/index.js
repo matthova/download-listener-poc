ee80051817fdce9f0f39274ab8aeaa9379e038db
matthova <matthova87@gmail.com>
2017-03-29 23:52:44 -0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// Hardcoded url to file
+var url = "http://lf.fusion360.io/api/limbforge?parameters=%7B%22component%22%3A1%2C%22orientation%22%3A%22left%22%2C%22C4%22%3A250%2C%22L1%22%3A250%2C%22TD%22%3A%22phone%22%7D";
+
+function fetchUrl() {
+  fetch(url, {method: 'get'})
+  .then(res => res.blob())
+  .then(blob => saveAs(blob, "filename.zip"))
+}
+
+function vanillaJs() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = "blob";
+  xhr.withCredentials = true;
+  xhr.onreadystatechange = function (){
+      if (xhr.readyState === 4) {
+          var blob = xhr.response;
+          FileSaver.saveAs(blob, filename);
+      }
+  };
+  xhr.send();
+}
+
+function ajaxDownload() {
+  $.ajax({
+    type: "GET",
+    url: url,
+    cache: false,
+    contentType: "jsonp",
+    success: (data) => {
+      var byteArray = (data);
+      var blob = new Blob([byteArray], { type: "application/octet-stream" });
+      var fileName = "download.zip";
+      saveAs(blob, fileName);
+    }
+  });
+}
+
+$(document).on('ready', function() {
+  $("#our-button").click(vanillaJs);
+  // $("#our-button").click(fetchUrl);
+  // $("#our-button").click(ajaxDownload);
+});
